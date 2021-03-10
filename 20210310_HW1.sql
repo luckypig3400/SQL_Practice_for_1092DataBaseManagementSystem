63b23c45d8f2b1728a0de81fb3329523cc3a28db
郵差 <a34007024@gmail.com>
2021-03-10 11:10:25 +0800

    simplify columns name

--- 20210310_HW1.sql
+++ 20210310_HW1.sql
@@ -14,47 +14,47 @@ if OBJECT_ID('Account')IS NOT NULL begin
 	drop table Account;
 end
 create table Account(
-	BankCodename char(6),
-	AccountID char(60),
+	BankID char(6),
+	AccID char(60),
 	Balance int,
-	BranchAccount char(60),
+	BranchAcc char(60),
 	AccountType int,
-	PRIMARY KEY(AccountID)
+	PRIMARY KEY(AccID)
 );
 
 if OBJECT_ID('Customer')IS NOT NULL begin
 	drop table Customer;
 end
 create table Customer(
-	AccountID char(69),
+	AccID char(69),
 	FirstName char(32),
 	LastName char(32),
 	Birthdate date,
 	Gender char(3),
 	PostalAddress char(255),
-	PRIMARY KEY(AccountID)
+	PRIMARY KEY(AccID)
 );
 
 if OBJECT_ID('TransactionLog')IS NOT NULL begin
 	drop table TransactionLog;
 end
 create table TransactionLog(
-	AccountID char(69),
-	TransactionID char(60),
-	TransactionDate date,
+	AccID char(69),
+	traID char(60),
+	traDate date,
 	ATM_ID char(60),
-	TransactionType char(32),
-	TransactionDetail char(255),
-	PRIMARY KEY(AccountID,TransactionID)
+	traType char(32),
+	traDetail char(255),
+	PRIMARY KEY(AccID,traID)
 );
 
 if OBJECT_ID('Bank')IS NOT NULL begin
 	drop table Bank;
 end
 create table Bank(
-	BankCodename char(6),
+	BankID char(6),
 	BankName char(66),
-	PRIMARY KEY(BankCodename)
+	PRIMARY KEY(BankID)
 );
 
 
@@ -63,21 +63,21 @@ alter table Customer drop column Gender;--刪除欄位
 alter table Customer add Gender char(1);--新增欄位
 alter table Customer add constraint Gender DEFAULT 'U' FOR Gender;--在欄位新增約束並加入預設值
 
-insert into Account(BankCodename,AccountID,Balance,BranchAccount,AccountType)
+insert into Account(BankID,AccID,Balance,BranchAcc,AccountType)
 values ('012','082214226',60000,'00412',1);
-insert into Account(BankCodename,AccountID,Balance,BranchAccount,AccountType)
+insert into Account(BankID,AccID,Balance,BranchAcc,AccountType)
 values ('012','082214266',50000,'00412',1);
-insert into Customer(AccountID,FirstName,LastName,Birthdate,Gender,PostalAddress)
+insert into Customer(AccID,FirstName,LastName,Birthdate,Gender,PostalAddress)
 values ('082214226','曜承','楊','2000/07/13','M','地址範例1');
-insert into Customer(AccountID,FirstName,LastName,Birthdate,Gender,PostalAddress)
+insert into Customer(AccID,FirstName,LastName,Birthdate,Gender,PostalAddress)
 values ('082214266','測試','洪','1996/06/06','M','地址範例2');
-insert into TransactionLog(AccountID,TransactionID,TransactionDate,ATM_ID,TransactionType,TransactionDetail)
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail)
 values ('082214226','1',GETDATE(),'1','1','開戶存款');
-insert into TransactionLog(AccountID,TransactionID,TransactionDate,ATM_ID,TransactionType,TransactionDetail)
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail)
 values ('082214266','1',GETDATE(),'1','1','開戶存款');
-insert into Bank(BankCodename,BankName)
+insert into Bank(BankID,BankName)
 values ('012','北護銀行');
-insert into Bank(BankCodename,BankName)
+insert into Bank(BankID,BankName)
 values ('013','北科銀行');
 
-select * from Account As A, Customer AS C WHERE A.AccountID = C.AccountID;
+select * from Account As A, Customer AS C WHERE A.AccID = C.AccID;
