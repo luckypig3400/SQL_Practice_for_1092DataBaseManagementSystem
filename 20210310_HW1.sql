de2b00b99248d08e4e08e5277178c9770d349f37
郵差 <a34007024@gmail.com>
2021-03-10 12:30:47 +0800

    insert 24 different data

--- 20210310_HW1.sql
+++ 20210310_HW1.sql
@@ -72,22 +72,57 @@ alter table Customer drop column Gender;--刪除欄位
 alter table Customer add Gender char(1);--新增欄位
 alter table Customer add constraint Gender DEFAULT 'U' FOR Gender;--在欄位新增約束並加入預設值
 
-insert into Account(BankID,AccID,Balance,BranchAccID,AccType)
-values ('012','082214226',60000,'00412','活期存款戶');
-insert into Account(BankID,AccID,Balance,BranchAccID,AccType)
-values ('012','082214266',50000,'00412','活期存款戶');
-insert into Customer(AccID,FirstName,LastName,Birthdate,Gender,PostalAddress)
-values ('082214226','曜承','楊','2000/07/13','M','地址範例1');
-insert into Customer(AccID,FirstName,LastName,Birthdate,Gender,PostalAddress)
-values ('082214266','測試','洪','1996/06/06','M','地址範例2');
-insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail)
-values ('082214226','1',GETDATE(),'1','1','開戶存款');
-insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail)
-values ('082214266','1',GETDATE(),'1','1','開戶存款');
-insert into Bank(BankID,BankName)
-values ('012','北護銀行');
-insert into Bank(BankID,BankName)
-values ('013','北科銀行');
+insert into Account(BankID,AccID,Balance,BranchAccID,AccType,UpdateDate,ChangedPersonnel)
+values ('012','082214226',60000,'00333','活期存款戶',GETDATE(),'楊曜承');
+insert into Account(BankID,AccID,Balance,BranchAccID,AccType,UpdateDate,ChangedPersonnel)
+values ('013','082214226666',3000000,'00333','定期存款戶',GETDATE(),'楊曜承');
+insert into Account(BankID,AccID,Balance,BranchAccID,AccType,UpdateDate,ChangedPersonnel)
+values ('012','07876467',60000,'00412','活期存款戶',GETDATE(),'測試一');
+insert into Account(BankID,AccID,Balance,BranchAccID,AccType,UpdateDate,ChangedPersonnel)
+values ('013','08435162776',30000,'00412','定期存款戶',GETDATE(),'測試一');
+insert into Account(BankID,AccID,Balance,BranchAccID,AccType,UpdateDate,ChangedPersonnel)
+values ('013','896787653',660000,'00412','活期存款戶',GETDATE(),'江戶川柯南');
+
+insert into Customer(AccID,FirstName,LastName,Birthdate,Gender,PostalAddress,City,Country,UpdateDate,ChangedPersonnel)
+values ('082214226','曜承','楊','2000/07/13','M','地址範例1','台北','台灣',GETDATE(),'楊曜承');
+insert into Customer(AccID,FirstName,LastName,Birthdate,Gender,PostalAddress,City,Country,UpdateDate,ChangedPersonnel)
+values ('07876467','一','測試','1990/06/03','M','地址範例2','台北','台灣',GETDATE(),'測試一');
+insert into Customer(AccID,FirstName,LastName,Birthdate,Gender,PostalAddress,City,Country,UpdateDate,ChangedPersonnel)
+values ('896787653','柯南','江戶川','1999/06/9','M','Tokyo Hot Street 69','東京','日本',GETDATE(),'江戶川柯南');
+
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('082214226','1',GETDATE(),'3','存款','開戶存款',GETDATE(),'楊曜承');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('082214226','2',GETDATE(),'3','提款','測試提款',GETDATE(),'楊曜承');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('082214226','3',GETDATE(),'3','存款','購屋頭款預備金',GETDATE(),'楊曜承');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('07876467','1',GETDATE(),'3','存款','開戶存款',GETDATE(),'測試一');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('07876467','2',GETDATE(),'3','存款','dwfesgtrgtrh',GETDATE(),'測試一');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('07876467','3',GETDATE(),'3','存款','etwretrdwfesgtrgtrh',GETDATE(),'測試一');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('07876467','4',GETDATE(),'3','提款','hgfhrhgfhytdh',GETDATE(),'測試一');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('07876467','5',GETDATE(),'3','提款','yjuyj',GETDATE(),'測試一');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('07876467','6',GETDATE(),'6','提款','otho',GETDATE(),'測試一');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('07876467','7',GETDATE(),'6','提款','???what???',GETDATE(),'測試一');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('07876467','8',GETDATE(),'6','存款','QwQ OuO',GETDATE(),'測試一');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('896787653','1',GETDATE(),'33','存款','開戶存款',GETDATE(),'江戶川柯南');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('896787653','2',GETDATE(),'67431','提款','偵查工具數件',GETDATE(),'江戶川柯南');
+insert into TransactionLog(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
+values ('896787653','3',GETDATE(),'69696','存款','破案報酬',GETDATE(),'江戶川柯南');
+
+insert into Bank(BankID,BankName,BankAddress)
+values ('012','北護銀行','台北市北投區明德路365號');
+insert into Bank(BankID,BankName,BankAddress)
+values ('013','地球銀行','地球的每個角落');
 
 
 select * from Account As A, Customer AS C WHERE A.AccID = C.AccID;
