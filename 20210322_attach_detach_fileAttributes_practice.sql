eae9d19dd0c7a17679f17b518672bd413fda9381
郵差 <a34007024@gmail.com>
2021-03-22 16:18:03 +0800

    attach database and set DB file attributes

--- 20210322_attach_detach_fileAttributes_practice.sql
+++ 20210322_attach_detach_fileAttributes_practice.sql
@@ -9,3 +9,13 @@ use practiceDB;
 use master;--切換到其他資料庫中再執行
 EXEC sp_detach_db practiceDB;
 
+--2. 附加資料庫
+CREATE DATABASE practiceDB
+ON PRIMARY
+  ( NAME='practiceDB',
+    FILENAME=
+       'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\practiceDB.mdf',
+    SIZE=4MB,
+    MAXSIZE=10MB,
+    FILEGROWTH=1MB)
+FOR ATTACH;
