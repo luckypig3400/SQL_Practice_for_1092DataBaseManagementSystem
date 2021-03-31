f4da603fa23baa95b47d95f02f0c7d18b54c34e5
郵差 <a34007024@gmail.com>
2021-03-31 10:41:22 +0800

    import teacher's table configuration and data

--- 20210322_HW1/CreateDB.sql
+++ 20210322_HW1/CreateDB.sql
@@ -6,18 +6,14 @@
 1. CreateDB.sql: CREATE TABLE規格: (合計共80分)
  - 1.1. 新增資料庫，需包含用指定特定資料檔的方式(附加資料庫FOR ATTACH) ，須提供bank.mdf(15分,bank.mdf 15分，合計30分)
   * 提示: 需先將資料庫bank.mdf檔建立起來，再用FOR ATTACH載入MDF檔。bank.mdf 。路徑請指定為 D:\MSSQL_DB\bank.mdf
- - 1.2. 每個資料表需建立個別的PRIMARY KEY (每個表各5分，共15分)
- - 1.3  將[帳號], [個人資訊]這兩個資料表以[個人帳號ID]欄位進行Foreigen Key 關聯(每個表各10分，共20分)
- - 1.4  在 [個人資訊]資料表中加入限制開戶應超過18歲才能開戶(才能建檔) (5分)
- - 1.5  在[個人資訊]的[性別]欄位中，設定預設值為'U'  (5分)
- - 1.6  在[個人資訊]的ID設定資料型態為int,且為PRIMARY KEY，且預設初始值為100000, 當新增資料時，每次數值自動加2 (5分)
 */
 use master;
 
 if DB_ID('BANK')IS NOT NULL begin
 	drop database BANK;
-end
---sql語法中的begin與end就如同C或JAVA的{}把判斷式成立後要執行的多個程式碼框起來
+end--sql語法中的begin與end就如同C或JAVA的{}把判斷式成立後要執行的多個程式碼框起來
+
+--以下建立指定路徑儲存及其他參數設定的資料庫
 create database BANK
 	ON PRIMARY(
 		name='BANK',
@@ -48,177 +44,104 @@ create database BANK
 
 use BANK;
 
-drop table if exists Customer;--SQL Server2016開始支援 刪除已存在的資料
-create table Customer
-(--在練習中稱為個人資料
-	userID char(66) primary key,--個人帳號似乎和銀行帳號式不同的
-	--應該是指這個人在這間店的顧客編號
-	--客戶編號應為主鍵(如身分證字號不可重複)
-	AccID char(69),
-	FirstName char(32),
-	LastName char(32),
-	Birthdate date,
-	Gender char(3),
-	PostalAddress char(255),
-	City char(30),
-	Country char(30),
-	UpdateDate date,
-	ChangedPersonnel char(60),--異動人
-);
+/*
+ - 1.2. 每個資料表需建立個別的PRIMARY KEY (每個表各5分，共15分)
+ - 1.3  將[帳號], [個人資訊]這兩個資料表以[個人帳號ID]欄位進行Foreigen Key 關聯(每個表各10分，共20分)
+ - 1.4  在 [個人資訊]資料表中加入限制開戶應超過18歲才能開戶(才能建檔) (5分)
+ - 1.5  在[個人資訊]的[性別]欄位中，設定預設值為'U'  (5分)
+ - 1.6  在[個人資訊]的ID設定資料型態為int,且為PRIMARY KEY，且預設初始值為100000, 當新增資料時，每次數值自動加2 (5分)
+*/
 
-if OBJECT_ID('Bank')IS NOT NULL begin
-	drop table Bank;
-end
-create table Bank
+-- 新增用戶資料Table: Customer 
+CREATE TABLE Customer
 (
-	BankID char(6),
-	BankName char(66),
-	BankAddress char(255),
-	PRIMARY KEY(BankID),
-);
+  ID int,
+  LName varchar(20),
+  FName varchar(20),
+  BDate date,
+  Sex char(1),
+  Address varchar(50),
+  City varchar(20),
+  Country varchar(50),
+  UP_Date datetime,
+  UP_User int,
+  PRIMARY KEY (ID)
+)
+GO
 
-if OBJECT_ID('Account')IS NOT NULL begin
-	drop table Account;
-end
-create table Account
+DECLARE @CURRENT_TS datetimeoffset = GETDATE()
+INSERT INTO Customer
+  (ID,Lname,FName,BDate,Sex,Address,City,Country,UP_Date,UP_User)
+VALUES('0', 'CY', 'Lien', '19120101', 'M', 'Neihu', 'Taipei', 'Taiwan', @CURRENT_TS, '0');
+INSERT INTO Customer
+  (ID,Lname,FName,BDate,Sex,Address,City,Country,UP_Date,UP_User)
+VALUES('001', 'LJ', 'KUO', '19981002', 'F', 'Neihu', 'Taipei', 'Taiwan', @CURRENT_TS, '0');
+INSERT INTO Customer
+  (ID,Lname,FName,BDate,Sex,Address,City,Country,UP_Date,UP_User)
+VALUES('002', 'CW', 'Lin', '19981002', 'F', 'Tianmu', 'Taipei', 'Taiwan', @CURRENT_TS, '0');
+INSERT INTO Customer
+  (ID,Lname,FName,BDate,Sex,Address,City,Country,UP_Date,UP_User)
+VALUES('003', 'DW', 'Wang', '19981002', 'M', 'Beitou', 'Taipei', 'Taiwan', @CURRENT_TS, '0');
+GO
+
+-- 新增銀行帳戶Table: Account 
+CREATE TABLE Account
 (
-	BankID char(6),
-	AccID char(69),
-	Balance int,
-	BranchAccID char(60),
-	AccType char(30),
-	UpdateDate date,
-	ChangedPersonnel char(60),--異動人
-	cID char(66),--要進行關聯，資料格式與長度必須與要關聯的欄位相同
-	--customerID記錄這隻銀行帳戶是誰開的連結到Customer表單
-	PRIMARY KEY(AccID),
-	foreign key(cID) references Customer(userID),
-	Foreign Key(BankID) References Bank(BankID)
-);
+  ID int,
+  AccID varchar(10) PRIMARY KEY,
+  Balance int,
+  BranchID int,
+  AccType varchar(3),
+  UP_Date datetime,
+  UP_User int,
+) ON BANK_FG1;
+GO
 
-if OBJECT_ID('TransactionLog')IS NOT NULL begin
-	drop table TransactionLog;
-end
-create table TransactionLog
+-- 插入測試資料
+DECLARE @CURRENT_TS datetimeoffset = GETDATE()
+INSERT INTO ACCOUNT
+  (ID, AccID, Balance, BranchID, AccType, UP_Date, UP_User)
+VALUES('001', '00000001', '5000', '010', 'B01', @CURRENT_TS, '0');
+GO
+
+-- 新增交易紀錄Table: Trans 
+CREATE TABLE Trans
 (
-	AccID char(69),
-	traID char(60),
-	traDate date,
-	ATM_ID char(60),
-	traType char(32),
-	traDetail char(255),
-	UpdateDate date,
-	ChangedPersonnel char(60),--異動人
-	traAccID char(69),--轉帳對象的銀行帳號
-	PRIMARY KEY(AccID,traID),
-	Foreign Key(AccID) references Account(AccID)
-);
+  AccID varchar(10),
+  TranID int,
+  TranTime datetime,
+  AtmID varchar(3),
+  TranType varchar(3),
+  TranNote varchar(100),
+  UP_DATETIME datetime,
+  UP_USR int
+) ON BANK_FG1;
+GO
 
+-- 插入測試資料
+DECLARE @CURRENT_TS datetimeoffset = GETDATE()
+INSERT INTO Trans
+  (AccID, TranID, TranTime, AtmID, TranType, TranNote, UP_DATETIME, UP_USR)
+VALUES('00000001', '001', @CURRENT_TS, 'A01', 'A00', 'I love writing SQL because it makes me happy', @CURRENT_TS, '001');
+INSERT INTO Trans
+  (AccID, TranID, TranTime, AtmID, TranType, TranNote, UP_DATETIME, UP_USR)
+VALUES('00000001', '002', @CURRENT_TS, 'A01', 'B01', 'I love writing Java because it makes me AAAAAA', @CURRENT_TS, '001');
+INSERT INTO Trans
+  (AccID, TranID, TranTime, AtmID, TranType, TranNote, UP_DATETIME, UP_USR)
+VALUES('00000001', '003', @CURRENT_TS, 'A01', 'C01', 'B: Good night, Wellcome to Restaurant DATABASE may I help your ?', @CURRENT_TS, '001');
+INSERT INTO Trans
+  (AccID, TranID, TranTime, AtmID, TranType, TranNote, UP_DATETIME, UP_USR)
+VALUES('00000001', '004', @CURRENT_TS, 'A01', 'C02', 'C: Hi, we have 4 persons and wanna to CREATE a TABLE', @CURRENT_TS, '001');
+INSERT INTO Trans
+  (AccID, TranID, TranTime, AtmID, TranType, TranNote, UP_DATETIME, UP_USR)
+VALUES('00000001', '005', @CURRENT_TS, 'A01', 'C02', 'B: Sorry, there is no table available now.', @CURRENT_TS, '001');
+INSERT INTO Trans
+  (AccID, TranID, TranTime, AtmID, TranType, TranNote, UP_DATETIME, UP_USR)
+VALUES('00000001', '006', @CURRENT_TS, 'A01', 'D05', 'C: COW~ DROP a TABLE  (ノ° ロ °)ノ彡┻━┻!.', @CURRENT_TS, '001');
+INSERT INTO Trans
+  (AccID, TranID, TranTime, AtmID, TranType, TranNote, UP_DATETIME, UP_USR)
+VALUES('00000001', '007', @CURRENT_TS, 'A01', 'D05', 'B: AAA~~Now there is one TABLE available  ┬─┬ノ( º _ ºノ)....', @CURRENT_TS, '001');
 
-
---使用修改資料表的語法(alter)，將性別欄為設定限制條件，並新增預設值為U
-alter table Customer drop column Gender;--刪除欄位
-alter table Customer add Gender char(1);--新增欄位
-alter table Customer add constraint Gender DEFAULT 'U' FOR Gender;--在欄位新增約束並加入預設值
-
-
-
-insert into Customer
-	(userID,AccID,FirstName,LastName,Birthdate,Gender,PostalAddress,City,Country,UpdateDate,ChangedPersonnel)
-values
-	('yaocheng0822', '082214226', '曜承', '楊', '2000/07/13', 'M', '地址範例1', '台北', '台灣', GETDATE(), '楊曜承');
-insert into Customer
-	(userID,AccID,FirstName,LastName,Birthdate,Gender,PostalAddress,City,Country,UpdateDate,ChangedPersonnel)
-values
-	('test1userID', '07876467', '一', '測試', '1990/06/03', 'M', '地址範例2', '台北', '台灣', GETDATE(), '測試一');
-insert into Customer
-	(userID,AccID,FirstName,LastName,Birthdate,Gender,PostalAddress,City,Country,UpdateDate,ChangedPersonnel)
-values
-	('conan8967', '896787653', '柯南', '江戶川', '1999/06/9', 'M', 'Tokyo Hot Street 69', '東京', '日本', GETDATE(), '江戶川柯南');
-
-insert into Bank
-	(BankID,BankName,BankAddress)
-values
-	('012', '北護銀行', '台北市北投區明德路365號');
-insert into Bank
-	(BankID,BankName,BankAddress)
-values
-	('013', '地球銀行', '地球的每個角落');
-
-insert into Account
-	(BankID,AccID,Balance,BranchAccID,AccType,UpdateDate,ChangedPersonnel,cID)
-values
-	('012', '082214226', 60000, '00333', '活期存款戶', GETDATE(), '楊曜承', 'yaocheng0822');
-insert into Account
-	(BankID,AccID,Balance,BranchAccID,AccType,UpdateDate,ChangedPersonnel,cID)
-values
-	('013', '082214226666', 3000000, '00333', '定期存款戶', GETDATE(), '楊曜承', 'yaocheng0822');
-insert into Account
-	(BankID,AccID,Balance,BranchAccID,AccType,UpdateDate,ChangedPersonnel,cID)
-values
-	('012', '07876467', 60000, '00412', '活期存款戶', GETDATE(), '測試一', 'test1userID');
-insert into Account
-	(BankID,AccID,Balance,BranchAccID,AccType,UpdateDate,ChangedPersonnel,cID)
-values
-	('013', '08435162776', 30000, '00412', '定期存款戶', GETDATE(), '測試一', 'test1userID');
-insert into Account
-	(BankID,AccID,Balance,BranchAccID,AccType,UpdateDate,ChangedPersonnel,cID)
-values
-	('013', '896787653', 660000, '00412', '活期存款戶', GETDATE(), '江戶川柯南', 'conan8967');
-
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('082214226', '1', GETDATE(), '3', '存款', '開戶存款', GETDATE(), '楊曜承');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('082214226', '2', GETDATE(), '3', '提款', '測試提款', GETDATE(), '楊曜承');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('082214226', '3', GETDATE(), '3', '存款', '購屋頭款預備金', GETDATE(), '楊曜承');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('07876467', '1', GETDATE(), '3', '存款', '開戶存款', GETDATE(), '測試一');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('07876467', '2', GETDATE(), '3', '存款', 'dwfesgtrgtrh', GETDATE(), '測試一');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('07876467', '3', GETDATE(), '3', '存款', 'etwretrdwfesgtrgtrh', GETDATE(), '測試一');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('07876467', '4', GETDATE(), '3', '提款', 'hgfhrhgfhytdh', GETDATE(), '測試一');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('07876467', '5', GETDATE(), '3', '提款', 'yjuyj', GETDATE(), '測試一');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('07876467', '6', GETDATE(), '6', '提款', 'otho', GETDATE(), '測試一');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('07876467', '7', GETDATE(), '6', '提款', '???what???', GETDATE(), '測試一');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('07876467', '8', GETDATE(), '6', '存款', 'QwQ OuO', GETDATE(), '測試一');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('896787653', '1', GETDATE(), '33', '存款', '開戶存款', GETDATE(), '江戶川柯南');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('896787653', '2', GETDATE(), '67431', '提款', '偵查工具數件', GETDATE(), '江戶川柯南');
-insert into TransactionLog
-	(AccID,traID,traDate,ATM_ID,traType,traDetail,UpdateDate,ChangedPersonnel)
-values
-	('896787653', '3', GETDATE(), '69696', '存款', '破案報酬', GETDATE(), '江戶川柯南');
+SELECT * FROM Customer
+SELECT * FROM Account
+SELECT * FROM Trans
