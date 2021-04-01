bba9e044f85021665c190948aa765364bd2b99dc
郵差 <a34007024@gmail.com>
2021-04-01 17:02:12 +0800

    finish 2.5 alter 3 table (add default value for UP_Date column)

--- 20210322_HW1/AlterDB.sql
+++ 20210322_HW1/AlterDB.sql
@@ -2,7 +2,6 @@
 問題2 - ALTER Database
 以ALTER語法修改執行上述CreateDB.sql後的資料庫，規則說明如下
 2. AlterDB.sql: ALTER TABLE規格: (合計共70分)
- - 2.5 修改這三個資料表的[更新日期與時間]欄位，設定預設值為 "今天" (每個各10分，共15分)
  - 2.6 新增一資料表，為銀行[分行帳號資料表]內容包含: (1)分行帳號 (2)分行名稱  (5分)
  - 2.7 將上述2.6的 (1)分行帳號使用Alter語法使用Foreigen Key與
  [帳號資料表] 進行關聯，且不得破壞 2.3與1.3所建立的關聯性 (10分)
@@ -48,6 +47,14 @@ insert into Customer(ID,Lname,FName,BDate,Sex,Address,City,Country,UP_Date,UP_Us
 VALUES('007', 'Winnie', 'Pooh', '19530615', 'M', 'Winnie Road', 'Beijing', 'China', GETDATE(), '0');
 --test Data for 2.4
 
+-- 2.5 修改這三個資料表的[更新日期與時間]欄位，設定預設值為 "今天" (每個各10分，共15分)
+alter table Account add constraint DF__Account__UP_Date__useOperationDate
+default GETDATE() for UP_Date;
+alter table Customer add constraint DF__Customer__UP_Date__useOperationDate
+default GETDATE() for UP_Date;
+alter table Trans add constraint DF__Trans__UP_DATETIME__useOperationDate
+default GETDATE() for UP_DATETIME;
+exec sp_helpconstraint @objname = 'Trans';
 
 
 
