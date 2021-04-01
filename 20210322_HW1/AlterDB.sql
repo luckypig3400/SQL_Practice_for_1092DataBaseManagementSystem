f4867940d590129d39478180a6de73cb566582fe
郵差 <a34007024@gmail.com>
2021-04-01 16:07:41 +0800

    finish 2.4 alter Customer constraint from age to Country

--- 20210322_HW1/AlterDB.sql
+++ 20210322_HW1/AlterDB.sql
@@ -2,8 +2,6 @@
 問題2 - ALTER Database
 以ALTER語法修改執行上述CreateDB.sql後的資料庫，規則說明如下
 2. AlterDB.sql: ALTER TABLE規格: (合計共70分)
- - 2.4 修改 [個人資訊]資料表中超過18歲才能開戶的限制
- ，並變更為國籍為Taiwan才能開戶 (5分)
  - 2.5 修改這三個資料表的[更新日期與時間]欄位，設定預設值為 "今天" (每個各10分，共15分)
  - 2.6 新增一資料表，為銀行[分行帳號資料表]內容包含: (1)分行帳號 (2)分行名稱  (5分)
  - 2.7 將上述2.6的 (1)分行帳號使用Alter語法使用Foreigen Key與
@@ -29,10 +27,8 @@ use BANK;
 -- 2.2 修改在[個人資訊]的[性別]欄位中，將預設值改為'M' (5分)
 EXEC sp_helpconstraint @objname = 'Customer';
 --查詢物件(通常為Table)中的限制條件
-
 alter table Customer drop constraint DF__Customer__Sex__25869641;
 --把查詢到到的約束條件代號drop掉
-
 alter table Customer add constraint DF__Customer__Sex__666 default 'M'for Sex;
 --新增約束條件把Sex欄位的預設值設為M並且把該約束條件命名為DF__Customer__Sex__666
 
@@ -41,6 +37,17 @@ alter table Customer add constraint DF__Customer__Sex__666 default 'M'for Sex;
 alter table Trans add foreign key(AccID)
 references Account(AccID);
 --https://www.w3schools.com/sql/sql_foreignkey.asp
+
+-- 2.4 修改 [個人資訊]資料表中超過18歲才能開戶的限制
+-- ，並變更為國籍為Taiwan才能開戶 (5分)
+exec sp_helpconstraint @objname = 'Customer';
+alter table Customer drop constraint CK__Customer__BDate__24927208;
+alter table Customer add constraint CK__Customer__Country__tw check(Country = 'Taiwan');
+set identity_insert Customer on;--插入測資前先開啟允許插入自動編序欄位
+insert into Customer(ID,Lname,FName,BDate,Sex,Address,City,Country,UP_Date,UP_User)
+VALUES('007', 'Winnie', 'Pooh', '19530615', 'M', 'Winnie Road', 'Beijing', 'China', GETDATE(), '0');
+--test Data for 2.4
+
 
 
 
