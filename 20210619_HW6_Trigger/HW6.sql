082c3552813d9cd2a31664327ad9547154fbdab1
郵差 <twHenryYang@gmail.com>
2021-06-20 11:38:25 +0800

    finish Q1 auto update log after inserted new row

--- 20210619_HW6_Trigger/HW6.sql
+++ 20210619_HW6_Trigger/HW6.sql
@@ -1,5 +1,33 @@
+USE BankHW5;
 -- 說明: 請使用觸發(Trigger)修改[作業3]綜合語法，規則如下:
 
 -- 1.請使用AFTER觸發程序，改寫原本程式，在新增交易紀錄後，自動更新LOG_SEG流水號 (30)
+CREATE TRIGGER AutoUpdateLOG_SEQafterInsert
+ON Trans AFTER insert
+AS
+declare @formattedDate varchar(9) = CONVERT(varchar, GETDATE(), 112);
+declare @todayLogCount int = (select COUNT(SDATE) FROM LOG_SEQ WHERE SDATE = @formattedDate);
+declare @todayTransCount int = (select LOG_COUNT FROM LOG_SEQ WHERE SDATE = @formattedDate);
+IF @todayLogCount = 0 begin
+	print('此為今日的第一筆交易');
+	insert into LOG_SEQ values(@formattedDate , 1);
+end
+else if @todayTransCount = 999999 begin
+	print('很抱歉!已達本系統單日交易量上限，將取消此交易，請明日再來');
+	ROLLBACK
+end
+else begin
+	set @todayTransCount = @todayTransCount + 1;
+	print('此為今日的第' + CAST(@todayTransCount as varchar) + '筆交易');
+	update LOG_SEQ set LOG_COUNT = @todayTransCount where SDATE = @formattedDate;
+end
 
--- 2.請使用INSTEAD OF觸發程序，在操作新增交易紀錄時，不需要輸入TranID(範例格式: 20210524_000001)。將TranID新增至交易紀錄取代原本新增交易紀錄動作。並自動更新LOG_SEG流水號 (70)
+insert into Trans VALUES('6', '20210620_000013', GETDATE(), 'A03', 'D66', N'信義區60坪地契乙份', GETDATE(), '006');
+
+
+-- 2.請使用INSTEAD OF觸發程序，在操作新增交易紀錄時，不需要輸入TranID(範例格式: 20210524_000001)。
+--將TranID新增至交易紀錄取代原本新增交易紀錄動作。並自動更新LOG_SEG流水號 (70)
+
+
+
+use master;
